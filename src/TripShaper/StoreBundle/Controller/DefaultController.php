3b1d7138a77b33edb39cc97d373d4e2412bed325
Denis Rivals <denis.rivals@mazedia.fr>
2012-02-13 12:18:18 +0100

    Creation du StoreBundle - fonctionnement MongoDB

--- src/TripShaper/StoreBundle/Controller/DefaultController.php
+++ src/TripShaper/StoreBundle/Controller/DefaultController.php
@@ -10,28 +10,45 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+	/**
+	 * @Route("/hello/{name}")
+	 * @Template()
+	 */
+	public function indexAction($name)
+	{
+		return array('name' => $name);
+	}
 
-    /**
-     * @Route("/store/create")
-     * @Template()
-     */
+	/**
+	 * @Route("/store/create")
+	 * @Template()
+	 */
 	public function createAction()
 	{
-	    $resource = new Resource();
-	    $resource->setTitle('Resource ONE');
+		$resource = new Resource();
+		$resource->setTitle('Resource ONE');
 
-	    $dm = $this->get('doctrine.odm.mongodb.document_manager');
-	    $dm->persist($resource);
-	    $dm->flush();
+		$dm = $this->get('doctrine.odm.mongodb.document_manager');
+		$dm->persist($resource);
+		$dm->flush();
 
-	    return new Response('Created resource id '.$resource->getId());
+		return new Response('Created resource id '.$resource->getId());
+	}
+
+	/**
+	 * @Route("/store/show")
+	 * @Template()
+	 */
+	public function showAction($id)
+	{
+		$resource = $this->get('doctrine.odm.mongodb.document_manager')
+		->getRepository('TripShaperStoreBundle:Resource')
+		->find($id);
+
+		if (!$resource) {
+			throw $this->createNotFoundException('No resource found for id '.$id);
+		}
+
+		return new Response($resource->getTitle());
 	}
 }
